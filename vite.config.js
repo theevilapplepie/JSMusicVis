1469ad7903df88940c6879ae2436af50f469c37e
James V <theevilapplepie@gmail.com>
2025-12-23 03:16:41 -0500

    Fixing deployment issues.

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,24 @@ import { defineConfig } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
+  base: '',
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'music-metadata-browser': ['music-metadata-browser'],
+          'vendor': ['notyf', 'stats.js'],
+          'ionicons': ['ionicons/icons', 'ionicons'],
+          'webgpu': ['./webgpu-renderer.js']
+        }
+      }
+    }
   },
   optimizeDeps: {
     esbuildOptions: {
